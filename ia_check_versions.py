d3d41ba1c73a9d21836d50e8af23bf1ea1c324aa
Uminosachi <49424133+Uminosachi@users.noreply.github.com>
2023-07-05 19:26:08 +0900

    Check diffusers version and enable cpu offload

--- ia_check_versions.py
+++ ia_check_versions.py
@@ -1,23 +1,19 @@
-from packaging import version
+from packaging.version import parse
 from importlib.util import find_spec
-try:
-    from importlib.metadata import version as importlib_version
-    from functools import cached_property
-except:
-    from pkg_resources import get_distribution as importlib_version
-    cached_property = property
+from importlib.metadata import version
+from functools import cached_property
 
 def get_module_version(module_name):
-    module_version = None if find_spec(module_name) is None else importlib_version(module_name)
+    module_version = version(module_name) if find_spec(module_name) is not None else None
     return module_version
 
 def compare_version(version1, version2):
     if not isinstance(version1, str) or not isinstance(version2, str):
         return None
     
-    if version.parse(version1) > version.parse(version2):
+    if parse(version1) > parse(version2):
         return 1
-    elif version.parse(version1) < version.parse(version2):
+    elif parse(version1) < parse(version2):
         return -1
     else:
         return 0
@@ -25,13 +21,16 @@ def compare_version(version1, version2):
 def compare_module_version(module_name, version_string):
     module_version = get_module_version(module_name)
 
-    return compare_version(module_version, version_string)
+    result = compare_version(module_version, version_string)
+    return result if result is not None else -2
 
 class IACheckVersions:
     @cached_property
     def diffusers_enable_cpu_offload(self):
-        if (get_module_version("diffusers") is not None and compare_module_version("diffusers", "0.15.0") >= 0 and
-            get_module_version("accelerate") is not None and compare_module_version("accelerate", "0.17.0") >= 0):
+        print("diffusers version:", get_module_version("diffusers"))
+        print("accelerate version:", get_module_version("accelerate"))
+        if (find_spec("diffusers") is not None and compare_module_version("diffusers", "0.15.0") >= 0 and
+            find_spec("accelerate") is not None and compare_module_version("accelerate", "0.17.0") >= 0):
             return True
         else:
             return False
