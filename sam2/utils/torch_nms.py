3ddf311c6454a9e0f49d670559a73922419a0f8d
Uminosachi <49424133+Uminosachi@users.noreply.github.com>
2024-08-01 15:29:52 +0900

    Fix fallback nms to output same value as original

--- sam2/utils/torch_nms.py
+++ sam2/utils/torch_nms.py
@@ -3,13 +3,18 @@ from torchvision.ops.boxes import box_iou
 
 
 def nms(bboxes: torch.Tensor, scores: torch.Tensor, iou_threshold: float) -> torch.Tensor:
-    order = torch.argsort(-scores).to(bboxes.device)
-    indices = torch.arange(bboxes.shape[0]).to(bboxes.device)
-    keep = torch.ones_like(indices, dtype=torch.bool).to(bboxes.device)
-    for i in indices:
-        if keep[i]:
-            bbox = bboxes[order[i]]
-            iou = box_iou(bbox[None, ...], (bboxes[order[i + 1:]]) * keep[i + 1:][..., None])
-            overlapped = torch.nonzero(iou > iou_threshold)
-            keep[overlapped + i + 1] = 0
-    return order[keep]
+    order = torch.argsort(-scores)
+    keep = []
+
+    while order.numel() > 0:
+        i = order[0]
+        keep.append(i.item())
+
+        if order.numel() == 1:
+            break
+
+        ious = box_iou(bboxes[i].unsqueeze(0), bboxes[order[1:]])[0]
+        mask = ious <= iou_threshold
+        order = order[1:][mask]
+
+    return torch.tensor(keep, device=bboxes.device)
